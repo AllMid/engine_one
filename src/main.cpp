6f399b8ecf16c4ad4e5a50732aecabbaf124ac61
AllMid <dimagre15@gmail.com>
2025-05-24 13:33:04 +0300

    add_cmake_sfml_pattern

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    auto window = sf::RenderWindow(sf::VideoMode({800u, 600u}), "CMake SFML Project");
+    window.setFramerateLimit(144);
+    sf::CircleShape shape(300.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
